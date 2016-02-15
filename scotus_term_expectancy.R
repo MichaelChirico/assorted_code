b633cde25c23550921e8d773697fd187cfc50a34
Michael Chirico <MichaelChirico4@gmail.com>
2016-02-15 18:43:39 -0500

    scotus: adding alternative measures

--- scotus_term_expectancy.R
+++ scotus_term_expectancy.R
@@ -29,7 +29,14 @@ justices[ , c("term_begin", "term_end") :=
                               sub("[^–]*–", "", active))))]
 
 #approximate (discretized) length of term
-justices[ , term_length := term_end - term_begin]
+justices[ , c("term_length", "active") := 
+            .(term_end - term_begin, is.na(term_end))]
+justices[(active), term_length := 2016L - term_begin]
+justices[ , age_retired := term_end - born]
+justices[(active), age_retired := 2016L - born]
+
+#approximate (discretized) age at term onset
+justices[ , age_at_conf := term_begin - born]
 
 #cleanup footnotes; add corresponding color map
 justices[ , reason_left := gsub("\\[.*", "", reason_left)]
@@ -37,6 +44,7 @@ cols <- c("Currently serving" = "red",
           "Death" = "black", "Rejection" = "yellow",
           "Resignation" = "blue", "Retirement" = "darkgreen")
 
+# LIFE EXPECTANCY ####
 #regression:
 justices[(!alive), summary(reg <<- lm(age_at_death ~ born))]
 
@@ -55,18 +63,123 @@ CIs <- as.data.table(t(apply(replicate(
   BB, justices[(!alive)
                ##resample with replacement
                ][sample(.N, rep = TRUE),
-                 ##re-run regression andhttp://imgur.com/6TBYuta
+                 ##re-run regression and predict
                  predict(lm(age_at_death ~ born),
                          birth_range)]),
   1, quantile, c(.025, .05, .95, .975))))[ , born := birth_range$born]
 
 #plot:
-png("~/Desktop/scotus.png")
+png("~/Desktop/scotus_life_exp.png")
 justices[ , plot(born, age_at_death, col = cols[reason_left],
                  main = paste0("SCOTUS Justice Life Expectancy\n",
                                "With Bootstrapped CIs"),
                  xlab = "Year of Birth", pch = 19,
                  ylab = "Approximate Age at Death")]
+CIs[ , matplot(born, cbind(`2.5%`, `5%`, `95%`, `97.5%`), add = TRUE,
+               lty = 2, lwd = c(1,2,2,1), type = "l", col = "orange")]
+justices[ , abline(reg, col = "orange", lwd = 3)]
+legend("bottomright", horiz = TRUE, pch = 19, cex = .6,
+       col = cols, legend = names(cols),x.intersp=.5)
+dev.off()
+
+# TERM LENGTH ####
+#regression:
+justices[(!active), summary(reg <<- lm(term_length ~ born))]
+
+#Bootstrapped regression coefficient
+quantile(replicate(BB, justices[(!active)
+                                ][sample(.N, rep = TRUE),
+                                  lm(term_length ~ born)$coefficients["born"]]),
+         c(.005, .025, .05, .95, .975, .995))
+
+#Bootstrapped CIs
+## formatting magic
+CIs <- as.data.table(t(apply(replicate(
+  BB, justices[(!active)
+               ##resample with replacement
+               ][sample(.N, rep = TRUE),
+                 ##re-run regression and predict
+                 predict(lm(term_length ~ born),
+                         birth_range)]),
+  1, quantile, c(.025, .05, .95, .975))))[ , born := birth_range$born]
+
+#plot:
+png("~/Desktop/scotus_term_length.png")
+justices[ , plot(born, term_length, col = cols[reason_left],
+                 main = paste0("SCOTUS Justice Term Length\n",
+                               "With Bootstrapped CIs"),
+                 xlab = "Year of Birth", pch = 19,
+                 ylab = "Approximate Term Length")]
+CIs[ , matplot(born, cbind(`2.5%`, `5%`, `95%`, `97.5%`), add = TRUE,
+               lty = 2, lwd = c(1,2,2,1), type = "l", col = "orange")]
+justices[ , abline(reg, col = "orange", lwd = 3)]
+legend("bottomright", horiz = TRUE, pch = 19, cex = .6,
+       col = cols, legend = names(cols),x.intersp=.5)
+dev.off()
+
+# AGE AT RETIREMENT ####
+#regression:
+justices[(!active), summary(reg <<- lm(age_retired ~ born))]
+
+#Bootstrapped regression coefficient
+quantile(replicate(BB, justices[(!active)
+                                ][sample(.N, rep = TRUE),
+                                  lm(age_retired ~ born)$coefficients["born"]]),
+         c(.005, .025, .05, .95, .975, .995))
+
+#Bootstrapped CIs
+## formatting magic
+CIs <- as.data.table(t(apply(replicate(
+  BB, justices[(!active)
+               ##resample with replacement
+               ][sample(.N, rep = TRUE),
+                 ##re-run regression and predict
+                 predict(lm(age_retired ~ born),
+                         birth_range)]),
+  1, quantile, c(.025, .05, .95, .975))))[ , born := birth_range$born]
+
+#plot:
+png("~/Desktop/scotus_age_retired.png")
+justices[ , plot(born, age_retired, col = cols[reason_left],
+                 main = paste0("SCOTUS Justice Retirement Age\n",
+                               "With Bootstrapped CIs"),
+                 xlab = "Year of Birth", pch = 19,
+                 ylab = "Approximate Age at Term End")]
+CIs[ , matplot(born, cbind(`2.5%`, `5%`, `95%`, `97.5%`), add = TRUE,
+               lty = 2, lwd = c(1,2,2,1), type = "l", col = "orange")]
+justices[ , abline(reg, col = "orange", lwd = 3)]
+legend("bottomright", horiz = TRUE, pch = 19, cex = .6,
+       col = cols, legend = names(cols),x.intersp=.5)
+dev.off()
+
+# AGE AT TERM START ####
+#regression:
+justices[(!active), summary(reg <<- lm(age_at_conf ~ born))]
+
+#Bootstrapped regression coefficient
+quantile(replicate(BB, justices[(!active)
+                                ][sample(.N, rep = TRUE),
+                                  lm(age_at_conf ~ born)$coefficients["born"]]),
+         c(.005, .025, .05, .95, .975, .995))
+
+#Bootstrapped CIs
+## formatting magic
+CIs <- as.data.table(t(apply(replicate(
+  BB, justices[(!active)
+               ##resample with replacement
+               ][sample(.N, rep = TRUE),
+                 ##re-run regression and predict
+                 predict(lm(age_at_conf ~ born),
+                         birth_range)]),
+  1, quantile, c(.025, .05, .95, .975))))[ , born := birth_range$born]
+
+#plot:
+png("~/Desktop/scotus_age_onset.png")
+justices[ , plot(born, age_at_conf, col = cols[reason_left],
+                 main = paste0("SCOTUS Justice Age at Confirmation\n",
+                               "With Bootstrapped CIs"),
+                 xlab = "Year of Birth", pch = 19,
+                 ylab = "Approximate Age at Confirmation")]
 CIs[ , matplot(born, cbind(`2.5%`, `5%`, `95%`, `97.5%`), add = TRUE,
                lty = 2, lwd = c(1,2,2,1), type = "l", col = "orange")]
 justices[ , abline(reg, col = "orange", lwd = 3)]
