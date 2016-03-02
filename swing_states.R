5d6765571511e46f1780130fc984043969fcf64e
Michael Chirico <MichaelChirico4@gmail.com>
2016-03-02 11:30:22 -0500

    test html

--- swing_states.R
+++ swing_states.R
@@ -6,10 +6,14 @@ wiki <- "https://simple.wikipedia.org/wiki/List_of_U.S._states_by_population"
 states <- html(wiki) %>% html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>%
   html_table() %>% `[[`(1) %>% setDT()
 
-states[ , Population := as.numeric(gsub(",", "", Population))]
-states[ , State := gsub(intToUtf8(160), '', State)]
+setnames(states, c("rank", "state", "population",
+                   "house_seats", "electoral_votes",
+                   "pop_per_house", "pop_per_elec"))
+
+states[ , population := as.numeric(gsub(",", "", population))]
+states[ , state := gsub(intToUtf8(160), '', state)]
 
 swing_states <- c("Nevada", "Colorado", "Iowa", "Wisconsin",
                   "Ohio", "Pennsylvania", "New Hampshire",
                   "Virginia", "North Carolina", "Florida")
-states[ , sum(Population[State %in% swing_states]) / sum(Population)]
+states[ , sum(population[state %in% swing_states]) / sum(population)]
