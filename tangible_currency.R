658a82ad4b0a0a064fbe75de8dd05d8427701de5
Michael Chirico <MichaelChirico4@gmail.com>
2016-11-20 17:13:40 -0500

    some progress towards currency project

--- tangible_currency.R
+++ tangible_currency.R
@@ -20,12 +20,15 @@ exchange <-
 
 ref <- "USD"
 per <- paste0("per_", tolower(ref))
+#own exchange rate is 1
 exchange[symbol == ref, (per) := 1]
 css <- "#currency_value > div.sfe-break-bottom-4 > span.pr > span.bld"
 
 for (cur in setdiff(unique(exchange$symbol), ref)){
-  URL <- paste0("https://www.google.com/finance?q=USD", cur)
+  URL <- paste0("https://www.google.com/finance?q=", ref, cur)
+  # get value of one unit of ref currency in target currency
   x <- read_html(URL) %>% html_node(css) %>% html_text()
+  # output is formatted as "xxx.xxx cur" where cur is the symbol
   exchange[symbol == cur, (per) := as.numeric(gsub(cur, "", x))]
 }
 
@@ -40,7 +43,11 @@ inn <- paste0("l_in_", tolower(ref))
 denom[exchange, (inn) := 
         log2(x.denomination / get(paste0("i.", per))), on = "symbol"]
 
-denom[ , match := 
-         denom[symbol == ref][.SD, x.denomination, 
+denom[ , c("match", "match_image") := 
+         #self-join to only reference currency,
+         #  rolling to the nearest match (in log terms)
+         denom[symbol == ref][.SD, .(x.denomination, x.image_url),
                               on = "l_in_usd", roll = "nearest"],
        by = symbol]
+
+fwrite(denom, "currency_denominations_matched.csv")
