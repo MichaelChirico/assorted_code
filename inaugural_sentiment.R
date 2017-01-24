9c7793d04ff30e50f2a90f85148b0f085ed51907
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-23 20:19:34 -0500

    tweaking sentiment analysis

--- inaugural_sentiment.R
+++ inaugural_sentiment.R
@@ -13,20 +13,22 @@ past.urls = list.page %>%
   html_attr("href") %>% paste0("http://millercenter.org", .)
 
 positive_words = 
-  grep("^(;|$)", readLines("~/Documents/positive-words.txt"),
+  grep("^(;|$)", readLines("positive-words.txt"),
        invert = TRUE, value = TRUE)
 negative_words = 
-  grep("^(;|$)", readLines("~/Documents/negative-words.txt"),
+  grep("^(;|$)", readLines("negative-words.txt"),
        invert = TRUE, value = TRUE)
+
+punct.regex = "[,.:;—”\"()?!–]|--"
 
 sentiments = 
   rbindlist(lapply(past.urls, function(uu) {
     page = read_html(uu)
     speech = page %>% 
       html_nodes(xpath = '//div[@id="transcript"]/p') %>% 
-      html_text %>% gsub("[[:punct:]]", " ", .) %>% 
+      html_text %>% gsub(punct.regex, " ", .) %>% 
       paste(collapse = " ")
-    words = strsplit(speech, split = "\\s+")[[1L]]
+    words = tolower(strsplit(speech, split = "\\s+")[[1L]])
     date = page %>% 
       html_node(xpath = '//h1[@id="amprestitle"]') %>%
       html_text %>% gsub(".*\\((.*)\\).*", "\\1", .) %>% 
@@ -34,45 +36,43 @@ sentiments =
     president = page %>% 
       html_node(xpath = '//h1[@id="amprestitle"]/following-sibling::h2') %>%
       html_text
-    data.table(president, date, N_words = length(words),
-               N_positive = sum(words %in% positive_words),
-               N_negative = sum(words %in% negative_words),
-               source = uu)
+    data.table(president, date, words, source = uu)
   }))
 
 eisen.speech = read_html(eisen.url) %>%
   html_nodes(xpath = '//td/span/*') %>%
   html_text %>% paste(collapse = " ") %>%
   gsub("Note:.*", "", .) %>%
-  gsub("[[:punct:]]", " ", .)
+  gsub(punct.regex, " ", .)
 
-eisen.words = strsplit(eisen.speech, split = "\\s+")[[1L]]
+eisen.words = tolower(strsplit(eisen.speech, split = "\\s+")[[1L]])
 
 trump.speech = read_html(trump.url) %>% 
   html_nodes(xpath = paste('//div[@class="zn-body__paragraph"',
                            'and not(descendant::a)]')) %>%
   html_text %>% paste(collapse = " ") %>%
-  gsub("[[:punct:]]", " ", .)
+  gsub(punct.regex, " ", .)
 
-trump.words = strsplit(trump.speech, split = "\\s+")[[1L]]
+trump.words = tolower(strsplit(trump.speech, split = "\\s+")[[1L]])
 
 sentiments =
   rbind(sentiments,
         data.table(president = "Dwight D. Eisenhower",
                    date = as.Date("1957-01-21"),
-                   N_words = length(eisen.words),
-                   N_positive = sum(eisen.words %in% positive_words),
-                   N_negative = sum(eisen.words %in% negative_words),
+                   words = eisen.words, 
                    source = eisen.url),
         data.table(president = "Donald J. Trump",
                    date = as.Date("2017-01-20"),
-                   N_words = length(trump.words),
-                   N_positive = sum(trump.words %in% positive_words),
-                   N_negative = sum(trump.words %in% negative_words),
+                   words = trump.words,
                    source = trump.url))
 
 setorder(sentiments, date)
 
+sentiments[ , negative := words %in% negative_words]
+sentiments[ , positive := words %in% positive_words]
+
 sentiments[ , plot(date, N_words, col = rleid(president))]
-sentiments[ , plot(date, N_positive - N_negative)]
+sentiments[ , .(N_positive = sum(positive),
+                N_negative = sum(negative)), by = date
+            ][ , plot(date, N_positive - N_negative)]
                                                                  
